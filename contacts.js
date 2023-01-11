95b52eff799c5dd7ae6fa6699d6fe97fd9c42d06
Denys Barabanov <umbrach415@gmail.com>
2023-01-11 16:04:14 +0200

    create files

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+
+// Розкоментуйте і запиши значення
+ const contactsPath = path.join("./db/contacts.json");
+
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+}
+
+function getContactById(contactId) {
+}
+
+function removeContact(contactId) {
+}
+
+function addContact(name, email, phone) {
+}
+
